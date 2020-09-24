20d97b61d22f577f1b6cd037893ecfcbb8df55b2
Walter Hoerman <whoerman@gmail.com>
2020-09-24 11:06:28 -0400

    adding a navbar freme

--- portsmouthnh/src/App.js
+++ portsmouthnh/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from "./components/layouts/Navbar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Navbar />
         <p>
           Information about Portsmouth
         </p>
---
+++ portsmouthnh/src/components/layouts/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+
+        //no longer need render method because ot is a function now
+        return (
+            <nav className="navbar bg-primary">
+                <h1>
+                    Portsmouth, NH
+                </h1>
+            </nav>
+        )
+}
+
+export default Navbar
